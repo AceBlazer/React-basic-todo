1d17bdfec0f64a3209d4d4a4fc4601688a95c4f4
AceBlazer <diesel_1994@live.fr>
2019-10-14 01:23:14 +0200

    1st commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>Simple todo app to learn basics of ReactJS</p>
+        </React.Fragment>
+    )
+}
+
+export default About;
